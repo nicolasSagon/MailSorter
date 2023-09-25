e4f5d89a9f43e1237563b58e8e79580587db85a0
nicolasSagon <sagon.nicolas@gmail.com>
2023-09-25 15:02:05 +0200

    feat(convertToKotlinDsl): convert gradle files to kotlin dsl

---
+++ ui/home/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("kotlin-kapt")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.nicolas.sagon.home"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 24
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.2"
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation ("androidx.activity:activity-compose:1.7.2")
+    implementation ("androidx.core:core-ktx:1.10.1")
+    implementation (platform ("org.jetbrains.kotlin:kotlin-bom:1.8.0"))
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.9.0")
+    implementation (platform ("androidx.compose:compose-bom:2022.10.00"))
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation ("androidx.compose.ui:ui")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.ui:ui-tooling-preview")
+    implementation ("androidx.compose.material3:material3")
+
+    implementation ("com.google.android.gms:play-services-auth:20.6.0")
+
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-compiler:2.44")
+
+    implementation (project (":data:authentication"))
+    implementation (project (":ui:core"))
+    implementation (project (":domain:authentication"))
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation (platform ("androidx.compose:compose-bom:2022.10.00"))
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+}
